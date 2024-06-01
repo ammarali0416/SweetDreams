28c6fa9fe326d605bf039092ad83835ece68c59c
Ammar Syed Ali <ammarali0416@gmail.com>
2024-06-01 17:52:40 -0400

    - removed the old server directory

--- server_old/prompts/prompt_Summarize.js
+++
@@ -1,5 +0,0 @@
-export function generateSystemMessage(chapter) {
-    const systemMessage = `Provide a brief summary highlighting only the main plot points and key character developments from the chapter provided. Avoid unnecessary phrases like "in this chapter" and refrain from analyzing the text.
-${chapter}`
-    return systemMessage;
-}
